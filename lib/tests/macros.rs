cf7e23701a60aaa75a516f6d825f7ba01ad8e28a
Lyndon Brown <jnqnfe@gmail.com>
2018-10-16 05:35:42 +0100

    copyright header: "Version" → "version"

--- lib/tests/macros.rs
+++ lib/tests/macros.rs
@@ -2,7 +2,7 @@
 //
 // This file is part of the `gong` command-line argument processing library.
 //
-// Licensed under the MIT license or the Apache license (Version 2.0), at your option. You may not
+// Licensed under the MIT license or the Apache license (version 2.0), at your option. You may not
 // copy, modify, or distribute this file except in compliance with said license. You can find copies
 // of these licenses either in the LICENSE-APACHE and LICENSE-MIT files, or alternatively at
 // <http://opensource.org/licenses/MIT> and <http://www.apache.org/licenses/LICENSE-2.0>
