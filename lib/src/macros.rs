6e3771d36eb2606cf97ab0eb9e78e8491ffe61e6
Lyndon Brown <jnqnfe@gmail.com>
2018-10-19 21:05:09 +0100

    lib: update macro to point to new type locations

--- lib/src/macros.rs
+++ lib/src/macros.rs
@@ -8,7 +8,7 @@
 // <http://opensource.org/licenses/MIT> and <http://www.apache.org/licenses/LICENSE-2.0>
 // respectively.
 
-/// Construct an [`Options`](struct.Options.html)
+/// Construct an [`Options`](options/struct.Options.html)
 ///
 /// Takes:
 ///
@@ -28,23 +28,25 @@
 /// // Without modes, empty option lists
 /// let _ = gong_option_set!(vec![], vec![]);
 /// // With modes, empty option lists
-/// let _ = gong_option_set!(vec![], vec![], gong::OptionsMode::Standard, true);
+/// let _ = gong_option_set!(vec![], vec![], gong::options::OptionsMode::Standard, true);
 /// # }
 /// ```
 #[macro_export]
 macro_rules! gong_option_set {
     ( $long:expr, $short:expr, $mode:expr, $abbr:expr ) => {
-        $crate::Options { long: $long, short: $short, mode: $mode, allow_abbreviations: $abbr }
+        $crate::options::Options {
+            long: $long, short: $short, mode: $mode, allow_abbreviations: $abbr
+        }
     };
     ( $long:expr, $short:expr ) => {
-        $crate::Options {
-            long: $long, short: $short, mode: $crate::OptionsMode::Standard,
+        $crate::options::Options {
+            long: $long, short: $short, mode: $crate::options::OptionsMode::Standard,
             allow_abbreviations: true
         }
     };
 }
 
-/// Construct a [`LongOption`](struct.LongOption.html)
+/// Construct a [`LongOption`](options/struct.LongOption.html)
 ///
 /// Takes:
 ///
@@ -52,11 +54,13 @@ macro_rules! gong_option_set {
 /// 2. Boolean indicating whether or not it takes a data arg (optional, defaults to false)
 #[macro_export]
 macro_rules! gong_longopt {
-    ( $name:expr, $data:expr ) => { $crate::LongOption { name: $name, expects_data: $data } };
-    ( $name:expr ) => { $crate::LongOption { name: $name, expects_data: false } };
+    ( $name:expr, $data:expr ) => {
+        $crate::options::LongOption { name: $name, expects_data: $data }
+    };
+    ( $name:expr ) => { $crate::options::LongOption { name: $name, expects_data: false } };
 }
 
-/// Construct a [`ShortOption`](struct.ShortOption.html)
+/// Construct a [`ShortOption`](options/struct.ShortOption.html)
 ///
 /// Takes:
 ///
@@ -64,6 +68,6 @@ macro_rules! gong_longopt {
 /// 2. Boolean indicating whether or not it takes a data arg (optional, defaults to false)
 #[macro_export]
 macro_rules! gong_shortopt {
-    ( $ch:expr, $data:expr ) => { $crate::ShortOption { ch: $ch, expects_data: $data } };
-    ( $ch:expr ) => { $crate::ShortOption { ch: $ch, expects_data: false } };
+    ( $ch:expr, $data:expr ) => { $crate::options::ShortOption { ch: $ch, expects_data: $data } };
+    ( $ch:expr ) => { $crate::options::ShortOption { ch: $ch, expects_data: false } };
 }
